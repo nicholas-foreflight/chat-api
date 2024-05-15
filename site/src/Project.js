e2fdd6cedcfeb398307047c45aba5ab8c0687a0e
Nicholas Hopper <nicholas@foreflight.com>
2024-05-15 03:58:48 -0500

    Complete hack that works for Project

--- site/src/Project.js
+++ site/src/Project.js
@@ -4,7 +4,7 @@ import { Container} from 'semantic-ui-react';
 const Project = () => (
     <div style={{ height: '100vh', margin: '-4em', marginLeft: '-25em', marginRight: '-25em'}}>
         <iframe
-            src="/project.html"
+            src="https://codepen.io/Nicholas-Hopper/full/MWdwZmB"
             style={{ width: '100%', height: '100%', border: 'none' }}
             title="Project Page"
         />
